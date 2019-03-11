84229b90069d461392bf385293cb0d468c3241ec
Nazar Kalinchuk <nazariy2410@gmail.com>
2019-03-11 23:10:01 +0200

    circle generate

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,11 +5,21 @@ class Home extends Component{
     constructor(props){
         super(props);
         this.canvas = '';
+        this.ctx = '';
         this.width = window.innerWidth;
         this.height = window.innerHeight;
         this.halfX = this.width / 2;
         this.halfY = this.height / 2;
+
+        this.lines = [];
+        this.linesNumber = 4;
+        this.vertices = 100;
+        this.radius = 200;
+        this.color = '#342132';
+
         this.canvasInit = this.canvasInit.bind(this);
+        this.update = this.update.bind(this);
+        this.renderAnimation = this.renderAnimation.bind(this);
         this.raf = this.raf.bind(this);
     }
 
@@ -20,12 +30,41 @@ class Home extends Component{
 
     canvasInit(){
         this.canvas = this.refs.canvas;
-        let ctx = this.canvas.getContext('2d');
+        this.ctx = this.canvas.getContext('2d');
+        for(let i=0; i<= this.linesNumber; i++){
+            this.lines[i] = [];
+            for(let j=0; j<= this.vertices; j++){
+                let point = {
+                    x: Math.cos(j/this.vertices * Math.PI*2),
+                    y: Math.sin(j/this.vertices * Math.PI*2),
+                };
+                this.lines[i].push(point);
+            }
+        }
+        console.log(this.lines);
+
 
     }
 
-    raf(){
+    update(){
 
+    }
+
+    renderAnimation(){
+        this.ctx.clearRect(0,0,this.width, this.height);
+        this.ctx.strokeStyle = this.color;
+
+        for (let i=1; i <= this.vertices; i++){
+            this.ctx.beginPath();
+            this.ctx.moveTo(this.halfX + this.lines[i-1].x * this.radius, this.halfY + this.lines[i - 1].y * this.radius);
+            this.ctx.lineTo(this.halfX + this.lines[i].x * this.radius, this.halfY + this.lines[i].y * this.radius);
+            this.ctx.stroke();
+        }
+    }
+
+    raf(){
+        this.update();
+        this.renderAnimation();
         window.requestAnimationFrame(this.raf);
     }
 
